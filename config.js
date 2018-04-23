11986ad628e26153bd8cf558be896395e129094c
Evan Thompson <evanbrowningthompson@gmail.com>
2018-04-22 23:02:17 -0400

    created GET endpoint, currently broken

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || global.TEST_DATABAS
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.IBM_USERNAME = process.env.IBM_USERNAME;
+exports.IBM_PASSWORD = process.env.IBM_PASSWORD;
--- routes/audio.js
+++ routes/audio.js
@@ -4,6 +4,15 @@ const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+const fs = require('fs-extra');
+
+const { IBM_USERNAME, IBM_PASSWORD } = require('../config');
+
+const text_to_speech = new TextToSpeechV1({
+  username: IBM_USERNAME,
+  password: IBM_PASSWORD
+});
 
 const router = express.Router();
 
@@ -15,7 +24,17 @@ router.use(jsonParser);
 
 // endpoint for getting audio files
 router.get('/:text', localAuth, (req, res) => {
+  const params = {
+    text: req.body.text,
+    voice: 'en-US_AllisonVoice',
+    accept: 'audio/wav'
+  };
 
+  res.status(200).send(
+    text_to_speech.synthesize(params).on('error', function(err) {
+      console.log('Error:', err);
+    }).pipe(fs.createWriteStream('text.wav'))
+  );
 });
 
 module.exports = router;
