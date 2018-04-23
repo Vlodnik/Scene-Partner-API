3e90fa1c719d0dcf0256061a0bd6cacd2a68cf99
Evan Thompson <evanbrowningthompson@gmail.com>
2018-04-23 12:52:34 -0400

    work on audio GET endpoint

--- routes/audio.js
+++ routes/audio.js
@@ -24,17 +24,31 @@ router.use(jsonParser);
 
 // endpoint for getting audio files
 router.get('/:text', localAuth, (req, res) => {
+  console.log('Received request to audio GET endpoint');
   const params = {
     text: req.body.text,
     voice: 'en-US_AllisonVoice',
     accept: 'audio/wav'
   };
 
-  res.status(200).send(
-    text_to_speech.synthesize(params).on('error', function(err) {
-      console.log('Error:', err);
-    }).pipe(fs.createWriteStream('text.wav'))
-  );
+  const filePath = text_to_speech.synthesize(params).on('error', function(err) {
+    console.log('Error:', err);
+  }).pipe(fs.createWriteStream('text.wav'));
+  console.log(`The filePath is ${filePath}`);
+
+  res.writeHead(200, {
+    'Content-Type': 'audio/wav'
+  });
+
+  fs.readFile(filePath, function(err, data) {
+    response.send(data);
+  });
+
+  // res.status(200).send(
+  //   text_to_speech.synthesize(params).on('error', function(err) {
+  //     console.log('Error:', err);
+  //   }).pipe(fs.createWriteStream('text.wav'))
+  // );
 });
 
 module.exports = router;
