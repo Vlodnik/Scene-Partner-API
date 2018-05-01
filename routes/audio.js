d46333f1037670085e9b5673b5edab225bf8bbf3
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-01 17:01:48 -0400

    endpoint now saves audio file to S3

--- routes/audio.js
+++ routes/audio.js
@@ -5,10 +5,9 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
-// const { pipeline } = require('stream');
 const fs = require('fs-extra');
-// const ms = require('mediaserver');
 const AWS = require('aws-sdk');
+const stream = require('stream');
 const s3 = new AWS.S3();
 
 const {
@@ -38,34 +37,44 @@ router.use(jsonParser);
 router.post('/', (req, res) => {
   console.log('Received request to audio POST endpoint');
 
-  const filePath = `http://localhost:8080/${req.body.lineId}.mp3`;
-
-  if(fs.existsSync(`/tmp/${req.body.lineId}.mp3`)) {
-    return res.status(200).send(filePath);
-  }
-
-  const IBMparams = {
-    text: req.body.text,
-    voice: 'en-US_AllisonVoice',
-    accept: 'audio/mp3'
+  const getParams = {
+    Bucket: S3_BUCKET_NAME,
+    Key: `${req.body.lineId}.mp3`
   };
 
-  const s3params = {
-    Body: 'Hello world',
-    Bucket: S3_BUCKET_NAME,
-    Key: AWS_ACCESS_KEY_ID
-  }
-
-  // const audioFile = text_to_speech.synthesize(IBMparams)
-
-  s3.putObject(params, function(err, data) {
-    if(err) {
-      console.log(err);
+  s3.getObject(getParams, function(err, data) {
+    if(data) {
+      return res.status(200).send(`https://scene-partner-mp3s.s3.amazonaws.com/${req.body.lineId}.mp3`);
     }
-    console.log(data);
+
+    const IBMparams = {
+      text: req.body.text,
+      voice: 'en-US_AllisonVoice',
+      accept: 'audio/mp3'
+    };
+
+    text_to_speech.synthesize(IBMparams)
+      .on('error', function(err) {
+        console.log('Error synthesizing:', err);
+      })
+      .pipe(uploadFromStream(s3))
+      .on('finish', function(data) {
+        // res.status(201).send(filePath);
+      });
+
+    function uploadFromStream(s3) {
+      const pass = new stream.PassThrough();
+
+      const params = {Bucket: S3_BUCKET_NAME, Key: `${req.body.lineId}.mp3`, ACL: 'public-read', Body: pass};
+      s3.upload(params, function(err, data) {
+        // console.log(err, data);
+        res.status(201).send(data.Location)
+      });
+
+      return pass;
+    }
   });
 
-  //  THIS IS WORKING CODE, DON'T FORGET IT
   // text_to_speech.synthesize(IBMparams)
   //   .on('error', function(err) {
   //     console.log('Error synthesizing:', err);
